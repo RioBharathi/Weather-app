c67f5870f427634e2562ca60e4cea2c3fa18170a
BHARATHI <150118249+RioBharathi@users.noreply.github.com>
2025-03-09 11:36:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/weather-app/", // 👈 Replace with your GitHub repo name
+  base: "/Weather-app/", // 👈 Replace with your GitHub repo name
 });
 
